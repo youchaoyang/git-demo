96a87452b56abdc2d7ea3152d4bd05196ba96981
youcy <youcy@123.com>
2022-09-16 13:58:07 +0800

    git-demo 1

---
+++ src/main/java/com/youcy/gitDemo/gitTest/GitTest.java
@@ -0,0 +1,12 @@
+package com.youcy.gitDemo.gitTest;
+
+/**
+ * @Description:
+ * @Author cy
+ * @Date 2022/9/16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("这是一段测试");
+    }
+}
